f8cc071ac348edb5dd8766b4c377925b7309d918
Iliyan Teofilov <iliyanvt18@gmail.com>
2023-05-01 17:09:07 +0200

    Got a window to display

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 if [ ! -d "build" ]; then
     mkdir build
 fi
+bspc rule -a '*' -o state=floating      # REMOVE THIS EVENTUALLY
 g++ ./src/main.cpp -g -pedantic -o ./build/main -lSDL2
 ./build/main
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,34 @@
 int main() {
     // returns zero on success else non-zero
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
+        printf("Error initializing SDL: %s\n", SDL_GetError());
     }
     SDL_Window* win = SDL_CreateWindow("Voronoi",
                                        SDL_WINDOWPOS_CENTERED,
                                        SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
+                                       1000, 1000, SDL_WINDOW_SHOWN);
 
-    while (1);
+    if (win == NULL)
+        printf("SDL_CreateWindow error: %s\n", SDL_GetError());
+
+    bool quit = false;
+
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     return 0;
 }
